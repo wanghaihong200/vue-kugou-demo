206ee6cfacea4e413b898d920aef2719709b225d
王海虹 <296701193@qq.com>
2017-09-20 12:35:24 +0800

    Add files via upload

---
+++ vue-demo-kugou-master/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <HeadTop />
+    <div class="main">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </div>
+    <Player />
+	  <DetailPlayer />
+	</div>
+</template>
+
+<script>
+  import HeadTop from './components/header/Head'
+  import Player from './components/Player'
+  import DetailPlayer from './components/DetailPlayer'
+  export default {
+    components: {
+      HeadTop, Player, DetailPlayer
+    }
+  }
+</script>
+
---
+++ vue-demo-kugou-master/src/mixins/index.js
@@ -0,0 +1,28 @@
+module.exports = {
+  'PLAY_AUDIO':{
+    methods:{
+      playAudio(index){
+        var hash = this.songList[index].hash
+        var info = {
+          list: this.songList,
+          index: index
+        }
+        this.$store.commit('setListInfo', info)
+        this.$store.dispatch('getSong', hash)
+      }
+    }
+  }
+};
+//export default {
+//    methods: {
+//      playAudio(index){
+//        var hash = this.songList[index].hash
+//        var info = {
+//          list: this.songList,
+//          index: index
+//        }
+//        this.$store.commit('setListInfo', info)
+//        this.$store.dispatch('getSong', hash)
+//      }
+//    }
+//}
